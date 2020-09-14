cdc00a1c5489c3ace0d6cdd2e2091f01ce6670e8
nickzaf95 <nicholaszafiriou@gmail.com>
2020-09-14 13:09:35 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
+function findMinAndRemove(firstHalf, secondHalf) {
+    let minfirstHalf = firstHalf[ 0 ]
+    let minsecondHalf = secondHalf[ 0 ]
+
+    if (minfirstHalf < minsecondHalf) {
+        return firstHalf.shift()
+    } else {
+        return secondHalf.shift()
+    }
+}
+
+function findMinAndRemoveSorted(firstHalf) {
+    return firstHalf.shift()
+}
+
+function merge(firstHalf, secondHalf) {
+    let sorted = []
+    while (firstHalf.length != 0 && secondHalf.length != 0) {
+        let currentMin = findMinAndRemove(firstHalf, secondHalf)
+        sorted.push(currentMin)
+    }
+    return sorted.concat(firstHalf).concat(secondHalf)
+}
+
+function mergeSort(array) {
+    let midpoint = array.length / 2
+    let firstHalf = array.slice(0, midpoint)
+    let secondHalf = array.slice(midpoint, array.length)
+    if (array.length < 2) {
+        return array
+    } else {
+        return merge(mergeSort(firstHalf), mergeSort(secondHalf))
+    }
+}
+
+
+
 
